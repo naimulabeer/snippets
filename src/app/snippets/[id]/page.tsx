799af71485f8ecb6772b2af4a777309daa34a3d6
naimulabeer <naimul.h.abeer@gmail.com>
2024-01-01 05:23:16 +0600

    fetching particular records

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,4 +1,20 @@
-export default function SnippetShowPage(props: any) {
-  console.log(props);
+import { notFound } from "next/navigation";
+import { db } from "@/db";
+
+interface ShowProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function SnippetShowPage(props: ShowProps) {
+  const snippet = await db.snippet.findFirst({
+    where: { id: parseInt(props.params.id) },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+
   return <div>Show a snippet!</div>;
 }
