729fbb1720e6c9455e9a861c6e1442e50bac19bf
naimulabeer <naimul.h.abeer@gmail.com>
2024-01-01 22:13:11 +0600

    adding monaco editor

--- src/app/snippets/[id]/edit/page.tsx
+++ src/app/snippets/[id]/edit/page.tsx
@@ -1,10 +1,26 @@
+import SnippetEditForm from "@/components/snippet-edit-form";
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
 interface SnippetsEditPageProps {
   params: {
     id: string;
   };
 }
 
-export default function SnippetEditPage(props: SnippetsEditPageProps) {
+export default async function SnippetEditPage(props: SnippetsEditPageProps) {
   const id = parseInt(props.params.id);
-  return <div>Editing snippets for id #{id}</div>;
+
+  const snippet = await db.snippet.findFirst({
+    where: { id },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+  return (
+    <div className="text-3xl font-bold">
+      <SnippetEditForm snippet={snippet} />
+    </div>
+  );
 }
